3f667e3d9a27c172c24071d7ad366f5c29abaa38
Sam Lawton <sgl26@bath.ac.uk>
2024-09-19 16:08:19 +0100

    initialisation of the folder and started the design of navigation bar

---
+++ web/src/components/NavBar/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './navbar.css'
+import Profile from '../../assets/Profile.jpg'
+import { Link } from 'react-scroll'
+import emailSymbol from '../../assets/email.png'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <img src={Profile} alt='Profile' className='Profile'/>
+            <div className='desktopMenu'>
+                <Link className='desktopMenuListItem'>Home</Link>
+                <Link className='desktopMenuListItem'>About</Link>
+                <Link className='desktopMenuListItem'>Experience</Link>
+                <Link className='desktopMenuListItem'>Portfolio</Link>
+            </div>
+            <button className='desktopMenuBtn'>
+                <img src={emailSymbol} alt='desktopMenuImg' className='desktopMenuImg'/>Contact Me</button>
+        </nav>
+    )
+}
+
+export default Navbar
