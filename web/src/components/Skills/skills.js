e3351bfc3b9cc72b5eeba9b9ad09d5837aeb5b9c
Sam Lawton <sgl26@bath.ac.uk>
2024-12-03 16:17:40 +0000

    03/12 changes

--- web/src/components/Skills/skills.js
+++ web/src/components/Skills/skills.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import './skills.css'
-import Languages from '../../assets/Languages.png'
+import Cpp from '../../assets/C++.png'
+import Python from '../../assets/Python.png'
+import MATLAB from '../../assets/Matlab.png'
+import C from '../../assets/C.png'
+import SQL from '../../assets/SQL.png'
+import VBA from '../../assets/VBA.png'
 import UoB from '../../assets/uob-logo.png'
 
 const Skills = () => {
     return (
         <section id='skills'>
-            <span className='skillTitle'> About Me </span>
-            <span className='blurbSkills'>An ambitious individual, eager to learn whilst growing into a professional role in Software Engineering. I take leading roles in projects, but also take time to create a positive atmosphere within my team.</span>
+            <span className='skillTitle'>Skills</span>
+            <span className='blurbSkills'>As an aspiring software engineer, I have varying experience in a number of different languages and applications.</span>
             <div className='skillBars'>
                 <div className='skillBar'>
-                    <img src={Languages} alt='Languages' className='skillBarImg'/>
                     <div className='skillBarText'>
-                        <h2>Programming Languages</h2>
-                        <p>Strong: C++, Matlab, Python; Basic: C, SQL, VBA</p>
+                        <h2>Strong Programming Languages</h2>
+                        <p>C++: 5 years university experience.</p> 
+                        <p>Python: 2 years high intensity experience.</p> 
+                        <p>MATLAB: 5 years university experience.</p>
+                    </div>
+                    <img src={Cpp} alt='C++' className='skillBarImg'/>
+                    <img src={Python} alt='Python' className='skillBarImg'/>
+                    <img src={MATLAB} alt='MATLAB' className='skillBarImg'/>
+
+                </div>
+                <div className='skillBar'>
+                    <img src={C} alt='C' className='skillBarImg'/>
+                    <img src={SQL} alt='SQL' className='skillBarImg'/>
+                    <img src={VBA} alt='VBA' className='skillBarImg'/>
+                    <div className='skillBarText'>
+                        <h2>Beginner Programming Languages</h2>
+                        <p>C: 5 years limited university experience.</p> 
+                        <p>SQL: 3 months placement experience.</p> 
+                        <p>VBA: 3 months placement experience.</p>
                     </div>
 
                 </div>
                 <div className='skillBar'>
-                    <img src={UoB} alt='University?' className='skillBarImg'/>
                     <div className='skillBarText'>
-                        <h2>Education</h2>
-                        <p>First-Class Meng from The University of Bath. 4 A*s at A-Level</p>
-                    </div>
-
-                </div>
-                <div className='skillBar'>
-                    <img src={Languages} alt='Something Else?' className='skillBarImg'/>
-                    <div className='skillBarText'>
-                        <h2>Experience</h2>
-                        <p>Kaggle Competitions, Electrical Engineering Placement, Data Science Placement</p>
+                        <h2>Other Skills</h2>
+                        <p></p>
                     </div>
                 </div>
             </div>
