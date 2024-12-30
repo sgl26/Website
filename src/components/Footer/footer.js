7048b1ccd87111a01c51417471c8cef14d954b29
Sam Lawton <sgl26@bath.ac.uk>
2024-12-30 14:30:07 +0000

    Small changes

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className='footer'>
-            Copyright &#169; 2023 Smith Williams. All right recieved.
+            2024 Sam Lawton.
         </footer>
     );
 }
